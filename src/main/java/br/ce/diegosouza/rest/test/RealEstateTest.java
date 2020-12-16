3dbbc3e2c3d91a6ed2ba309ad838f50bfff7c358
diego souza <diegolsouzaa@gmail.com>
2020-12-15 21:38:12 -0300

    Create account insertion test

--- src/main/java/br/ce/diegosouza/rest/test/RealEstateTest.java
+++ src/main/java/br/ce/diegosouza/rest/test/RealEstateTest.java
@@ -1,14 +1,37 @@
 package br.ce.diegosouza.rest.test;
 
 import br.ce.diegosouza.rest.core.BaseTest;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class RealEstateTest extends BaseTest {
+
+    private String token;
+
+    @Before
+    public void login(){
+
+        Map<String, String> login = new HashMap<String, String>();
+        login.put("email", "diego@lauriano");
+        login.put("senha", "1234");
+
+        token =
+                given()
+                        .body(login)
+                        .when()
+                        .post("/signin")
+                        .then()
+                        .statusCode(200)
+                        .extract().path("token");
+    }
 
     @Test
     public void shouldDoNotAcessApiWithoutToken(){
@@ -22,19 +45,6 @@ public class RealEstateTest extends BaseTest {
 
     @Test
     public void shouldIncludeAccount(){
-        Map<String, String> login = new HashMap<String, String>();
-        login.put("email", "diego@lauriano");
-        login.put("senha", "1234");
-
-        String token =
-        given()
-            .body(login)
-        .when()
-            .post("/signin")
-        .then()
-            .statusCode(200)
-            .extract().path("token")
-        ;
 
         given()
                .header("Authorization", "JWT " + token)
@@ -48,7 +58,25 @@ public class RealEstateTest extends BaseTest {
 
     @Test
     public void shouldChangeAccount(){
+        given()
+                .header("Authorization", "JWT " + token)
+                .body("{\"nome\": \"conta alterada\"}")
+                .when()
+                .put("/contas/331859")
+                .then().log().all()
+                .statusCode(200).body("nome", is("conta alterada"));
+    }
 
+    @Test
+    public void shouldDoNotIncludeAccountWithSameName(){
 
+        given()
+                .header("Authorization", "JWT " + token)
+                .body("{\"nome\": \"conta alterada\"}")
+                .when()
+                .post("/contas")
+                .then()
+                .statusCode(400).body("error", is("Já existe uma conta com esse nome!"))
+        ;
     }
 }
